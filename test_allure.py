acdf50cc3feb47edbd9d7d0f9e0f2d554d0aa83c
Aniket Malusare <malusarea111@gmail.com>
2022-10-04 13:35:59 +0530

    Update test_allure.py

--- test_allure.py
+++ test_allure.py
@@ -9,9 +9,9 @@ def test_methB():
     print("This is method B")
     allureLogs("This is method B")
 
-@pytest.mark.skip
 def test_methC():
     print("This is method C")
+    assert 1==2
 
 def test_methD():
     print("This is method D")
